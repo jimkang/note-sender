4cf7f4b2197dffa770c632f0a589510c1f862950
Jim Kang <jimkang@gmail.com>
2018-08-27 20:57:49 -0400

    Hopefully, accept video files.~

---
+++ dom/reset-fields.js
@@ -0,0 +1,8 @@
+function resetFields() {
+  var fileInput = document.getElementById('media-file');
+  var textArea = document.querySelector('textarea');
+  fileInput.value = null;
+  textArea.value = null;
+}
+
+module.exports = resetFields;
