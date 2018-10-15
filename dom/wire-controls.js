9a9f087a2f2816774e308b8eea1f58f4b6eb8a27
Jim Kang <jimkang@gmail.com>
2018-10-14 20:17:25 -0400

    Load file if a file is already selected when the page is loaded.

--- dom/wire-controls.js
+++ dom/wire-controls.js
@@ -26,21 +26,24 @@ function wireControls({ saveNoteFlow }) {
   d3.select('#media-file').on('change', onMediaFileChange);
   d3.select('#rotate-button').on('click', canvasImageOps.rotateImage);
 
+  var file = getFile();
+  if (file) {
+    loadFile(file);
+  }
+
   function onSaveNote() {
     var note = objectFromDOM(document.getElementById('note-form'));
     var archive = document.getElementById('archive').value;
     var password = document.getElementById('password').value;
-    var files = document.getElementById('media-file').files;
-
-    var file;
-    if (files.length > 0) {
-      file = files[0];
-    }
-    saveNoteFlow({ note, archive, password, file });
+    saveNoteFlow({ note, archive, password, file: getFile });
   }
 
   function onMediaFileChange() {
     var file = this.files[0];
+    loadFile(file);
+  }
+
+  function loadFile(file) {
     var maxSideLength = +maxSideLengthField.value;
     if (file && file.type.startsWith('image/') && !isNaN(maxSideLength)) {
       canvasImageOps.loadFileToCanvas({ file, mimeType: file.type, maxSideLength });
@@ -52,6 +55,16 @@ function InsertIntoTextarea(text) {
   return function insertIntoTextarea() {
     noteArea.value = noteArea.value + text;
   };
+}
+
+function getFile() {
+  var files = document.getElementById('media-file').files;
+
+  var file;
+  if (files.length > 0) {
+    file = files[0];
+  }
+  return file;
 }
 
 module.exports = wireControls;
